ebb8731ccf8ff2d078e336eda0aa95d0ccb1207d
m-troja <mtroja98@gmail.com>
2025-08-07 21:21:03 +0200

    Refactor

--- src/main/java/com/mir/survey/SurveyApplication.java
+++ src/main/java/com/mir/survey/SurveyApplication.java
@@ -1,12 +1,10 @@
 package com.mir.survey;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
-@Slf4j
 @SpringBootApplication
 @PropertySources({
 	@PropertySource("classpath:application.properties")
